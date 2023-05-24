c04a16ae42c90da6cfb29bee25993ef055b8e0ae
vinjan <108977419+vinjan23@users.noreply.github.com>
2023-05-24 16:51:13 +0700

    Update Testnet.js

--- src/components/Testnet.js
+++ src/components/Testnet.js
@@ -5,7 +5,6 @@ import althea from "../assets/img/althea.png";
 import archway from "../assets/img/archway.png";
 import blockx from "../assets/img/blockx.png";
 import bonus from "../assets/img/bonus.png";
-import c4e from "../assets/img/c4e.png";
 import cascadia from "../assets/img/cascadia.png";
 import composable from "../assets/img/composable.png";
 import elys from "../assets/img/elys.png";
@@ -90,6 +89,76 @@ export const Testnet = () => {
       </div>
        </a>
    <a href="https://github.com/vinjan23/Testnet.Guide/tree/main/Blockxd" target="_blank">
+      <h5 style={{margin: 0, fontSize: '20px', color: '#FFFFFF'}}>SERVICE</h5>
+    </a>
+  </div>
+</div>
+
+<div className="item">
+  <div style={{display: 'flex', flexDirection: 'column', alignItems: 'center'}}>
+   <h3 style={{marginBottom: '10px'}}>bonus</h3>
+   <a href="https://explorer.vinjan.xyz/bonusblock/staking/bonusvaloper12maezlndx2zh4turertywjgm47xx7erhm2nmtm" target="_blank">
+     <div style={{borderRadius: '50%', width: '150px', height: '150px', overflow: 'hidden'}}>
+        <img src={bonus} alt="Image" style={{width: '100%', height: '100%'}} />
+      </div>
+       </a>
+   <a href="https://github.com/vinjan23/Testnet.Guide/tree/main/Bonusblock" target="_blank">
+      <h5 style={{margin: 0, fontSize: '20px', color: '#FFFFFF'}}>SERVICE</h5>
+    </a>
+  </div>
+</div>
+
+<div className="item">
+  <div style={{display: 'flex', flexDirection: 'column', alignItems: 'center'}}>
+   <h3 style={{marginBottom: '10px'}}>cascadia</h3>
+   <a href="https://explorer.vinjan.xyz/cascadia-testnet/staking/cascadiavaloper1zccclw862uthjqcvem0hlh6fp9nvwjgeh5e6wa" target="_blank">
+     <div style={{borderRadius: '50%', width: '150px', height: '150px', overflow: 'hidden'}}>
+        <img src={cascadia} alt="Image" style={{width: '100%', height: '100%'}} />
+      </div>
+       </a>
+   <a href="https://github.com/vinjan23/Testnet.Guide/tree/main/Cascadia" target="_blank">
+      <h5 style={{margin: 0, fontSize: '20px', color: '#FFFFFF'}}>SERVICE</h5>
+    </a>
+  </div>
+</div>
+
+<div className="item">
+  <div style={{display: 'flex', flexDirection: 'column', alignItems: 'center'}}>
+   <h3 style={{marginBottom: '10px'}}>composable</h3>
+   <a href="https://explorer.vinjan.xyz/composable-testnet/staking/banksyvaloper172gvyhdgy3wuh2rrkcm9g0u0qvrl8t5fww29dm" target="_blank">
+     <div style={{borderRadius: '50%', width: '150px', height: '150px', overflow: 'hidden'}}>
+        <img src={composable} alt="Image" style={{width: '100%', height: '100%'}} />
+      </div>
+       </a>
+   <a href="https://github.com/vinjan23/Testnet.Guide/tree/main/Composable" target="_blank">
+      <h5 style={{margin: 0, fontSize: '20px', color: '#FFFFFF'}}>SERVICE</h5>
+    </a>
+  </div>
+</div>
+
+<div className="item">
+  <div style={{display: 'flex', flexDirection: 'column', alignItems: 'center'}}>
+   <h3 style={{marginBottom: '10px'}}>elys</h3>
+   <a href="https://explorer.vinjan.xyz/elystestnet-1/staking/elysvaloper1jf02c7l9ezy30eflx0kgp54d0tpsj2zzhvmf30" target="_blank">
+     <div style={{borderRadius: '50%', width: '150px', height: '150px', overflow: 'hidden'}}>
+        <img src={elys} alt="Image" style={{width: '100%', height: '100%'}} />
+      </div>
+       </a>
+   <a href="https://github.com/vinjan23/Testnet.Guide/tree/main/Elys" target="_blank">
+      <h5 style={{margin: 0, fontSize: '20px', color: '#FFFFFF'}}>SERVICE</h5>
+    </a>
+  </div>
+</div>
+
+<div className="item">
+  <div style={{display: 'flex', flexDirection: 'column', alignItems: 'center'}}>
+   <h3 style={{marginBottom: '10px'}}>ojo</h3>
+   <a href="https://explorer.vinjan.xyz/ojo/staking/ojovaloper1yh4gytj3ktj2x444ndgdngquxt9lpnz3un89pz" target="_blank">
+     <div style={{borderRadius: '50%', width: '150px', height: '150px', overflow: 'hidden'}}>
+        <img src={ojo} alt="Image" style={{width: '100%', height: '100%'}} />
+      </div>
+       </a>
+   <a href="https://github.com/vinjan23/Testnet.Guide/tree/main/Ojo" target="_blank">
       <h5 style={{margin: 0, fontSize: '20px', color: '#FFFFFF'}}>SERVICE</h5>
     </a>
   </div>
