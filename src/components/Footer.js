9c6e64183b7aaf63d2f1af5332c29c608a616d2c
vinjan <108977419+vinjan23@users.noreply.github.com>
2023-05-24 19:50:06 +0700

    Update Footer.js

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -2,10 +2,8 @@ import React from 'react';
 import { Container, Row, Col } from "react-bootstrap";
 import { MailchimpForm } from "./MailchimpForm";
 import logo from "../assets/img/logo.svg";
-import navIcon1 from "../assets/img/discord.svg";
 import navIcon2 from "../assets/img/github.svg";
 import navIcon3 from "../assets/img/twitter.svg";
-import navIcon4 from '../assets/img/youtube.svg';
 import headerImg from "../assets/img/header-img.svg";
 import 'animate.css';
 import TrackVisibility from 'react-on-screen';
@@ -26,10 +24,8 @@ export const Footer = () => {
           </Col>
           <Col size={12} sm={6} className="text-center text-sm-end">
             <div className="social-icon">
-                <a href="https://discordapp.com/users/928575843641479198" target="_blank"><img src={navIcon1} alt="Discord" /></a>
                 <a href="https://github.com/vinjan23"target="_blank"><img src={navIcon2} alt="Github" /></a>
                 <a href="https://twitter.com/vjanwil" target="_blank"><img src={navIcon3} alt="Twitter" /></a>
-                <a href="https://youtube.com" target="_blank"><img src={navIcon4} alt="Youtube" /></a>
             </div>
             <p>Made with ❤️ Vinjan</p>
           </Col>
