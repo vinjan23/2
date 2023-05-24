0cc4278e99cb7683e38911c59d237ff4f3292828
vinjan <108977419+vinjan23@users.noreply.github.com>
2023-05-24 15:31:38 +0700

    Update Mainnet.js

--- src/components/Mainnet.js
+++ src/components/Mainnet.js
@@ -1,7 +1,8 @@
 import React from 'react';
 
 
-import 8ball from "../assets/img/8ball.png";
+import arkhadian from "../assets/img/arkhadian.png";
+import decentr from "../assets/img/decentr.png";
 
 import source from "../assets/img/source.png";
 
@@ -46,13 +47,27 @@ export const Mainnet = () => {
                         
 <div className="item">
   <div style={{display: 'flex', flexDirection: 'column', alignItems: 'center'}}>
-   <h3 style={{marginBottom: '10px'}}>8Ball</h3>
-   <a href="https://explorer.vinjan.xyz/8ball-mainnet/staking/8ballvaloper1jf02c7l9ezy30eflx0kgp54d0tpsj2zz3zmn8h" target="_blank">
+   <h3 style={{marginBottom: '10px'}}>arkhadian</h3>
+   <a href="https://explorer.vinjan.xyz/arkhadian-mainnet/staking/arkhvaloper1caclqqep2jprjn0evwyt3jkn6q3zxx4c80rzcj" target="_blank">
      <div style={{borderRadius: '50%', width: '150px', height: '150px', overflow: 'hidden'}}>
-        <img src={8ball} alt="Image" style={{width: '100%', height: '100%'}} />
+        <img src={arkhadian} alt="Image" style={{width: '100%', height: '100%'}} />
       </div>
        </a>
-   <a href="https://github.com/vinjan23/Mainnet/tree/main/8ball" target="_blank">
+   <a href="https://github.com/vinjan23/Mainnet/tree/main/Arkh" target="_blank">
+      <h5 style={{margin: 0, fontSize: '20px', color: '#FFFFFF'}}>SERVICE</h5>
+    </a>
+  </div>
+</div>
+
+<div className="item">
+  <div style={{display: 'flex', flexDirection: 'column', alignItems: 'center'}}>
+   <h3 style={{marginBottom: '10px'}}>decentr</h3>
+   <a href="https://explorer.vinjan.xyz/decentr-mainnet/staking/decentrvaloper18yze7cvnep7d2yj3cs4zfpvd4juphxrw3djwsz" target="_blank">
+     <div style={{borderRadius: '50%', width: '150px', height: '150px', overflow: 'hidden'}}>
+        <img src={decentr} alt="Image" style={{width: '100%', height: '100%'}} />
+      </div>
+       </a>
+   <a href="https://github.com/vinjan23/Mainnet/tree/main/Decentr" target="_blank">
       <h5 style={{margin: 0, fontSize: '20px', color: '#FFFFFF'}}>SERVICE</h5>
     </a>
   </div>
