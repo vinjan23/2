d26713f9b8b374d8f181bdabe83bc95aae989bb4
vinjan <108977419+vinjan23@users.noreply.github.com>
2023-05-24 19:15:05 +0700

    Update Mainnet.js

--- src/components/Mainnet.js
+++ src/components/Mainnet.js
@@ -1,12 +1,12 @@
 import React from 'react';
 
 
-import arkhadian from "../assets/img/arkhadian.png";
-import eightball from "../assets/img/eightball.png";
+import planq from "../assets/img/planq.png";
+import realio from "../assets/img/realio.png";
 import decentr from "../assets/img/decentr.png";
 import gitopia from "../assets/img/gitopia.png";
-import realio from "../assets/img/realio.png";
-import planq from "../assets/img/planq.png";
+import arkhadian from "../assets/img/arkhadian.png";
+import eightball from "../assets/img/eightball.png";
 import terp from "../assets/img/terp.png";
 
 import source from "../assets/img/source.png";
@@ -45,23 +45,23 @@ export const Mainnet = () => {
                     <div className="skill-bx wow zoomIn">
                             
                         <h2 class="archive-container"> Mainnet <span class="spinner-grow spinner-grow-sm text-success" role="status"></span></h2>
-                        <p>This my validator on various mainnet below! </p>                        
+                        <p>This my validator on various mainnet </p>                        
                         <br></br>
                         <br></br>
                         <Carousel responsive={responsive} infinite={true} className="owl-carousel owl-theme skill-slider">
-                        
-<div className="item">
+ 
+ <div className="item">
   <div style={{display: 'flex', flexDirection: 'column', alignItems: 'center'}}>
-   <h3 style={{marginBottom: '10px'}}>arkhadian</h3>
-   <a href="https://explorer.vinjan.xyz/arkhadian-mainnet/staking/arkhvaloper1caclqqep2jprjn0evwyt3jkn6q3zxx4c80rzcj" target="_blank">
+   <h3 style={{marginBottom: '10px'}}>planq</h3>
+   <a href="https://explorer.vinjan.xyz/planq-mainnet/staking/plqvaloper1csj5g5j4r682apvjckdk3qldkup4y0745djamf" target="_blank">
      <div style={{borderRadius: '50%', width: '150px', height: '150px', overflow: 'hidden'}}>
-        <img src={arkhadian} alt="Image" style={{width: '100%', height: '100%'}} />
+        <img src={planq} alt="Image" style={{width: '100%', height: '100%'}} />
       </div>
        </a>
-   <a href="https://github.com/vinjan23/Mainnet/tree/main/Arkh" target="_blank">
+   <a href="https://github.com/vinjan23/Mainnet/tree/main/Planq" target="_blank">
       <h5 style={{margin: 0, fontSize: '20px', color: '#FFFFFF'}}>SERVICE</h5>
     </a>
-    <a href="https://explorer.vinjan.xyz/arkhadian-mainnet/staking/arkhvaloper1caclqqep2jprjn0evwyt3jkn6q3zxx4c80rzcj" target="_blank">
+    <a href="https://explorer.vinjan.xyz/arkhadian-mainnet/staking/plqvaloper1csj5g5j4r682apvjckdk3qldkup4y0745djamf" target="_blank">
     <h5 style={{margin: 0, fontSize: '20px', color: '#FFFFFF'}}>EXPLORER</h5>
     </a>
   </div>
@@ -69,16 +69,16 @@ export const Mainnet = () => {
 
 <div className="item">
   <div style={{display: 'flex', flexDirection: 'column', alignItems: 'center'}}>
-   <h3 style={{marginBottom: '10px'}}>8 Ball</h3>
-   <a href="https://explorer.vinjan.xyz/8ball-mainnet/staking/8ballvaloper1jf02c7l9ezy30eflx0kgp54d0tpsj2zz3zmn8h" target="_blank">
+   <h3 style={{marginBottom: '10px'}}>realio</h3>
+   <a href="https://explorer.vinjan.xyz/realio-mainnet/staking/realiovaloper1csj5g5j4r682apvjckdk3qldkup4y07472gtkh" target="_blank">
      <div style={{borderRadius: '50%', width: '150px', height: '150px', overflow: 'hidden'}}>
-        <img src={eightball} alt="Image" style={{width: '100%', height: '100%'}} />
+        <img src={realio} alt="Image" style={{width: '100%', height: '100%'}} />
       </div>
        </a>
-   <a href="https://github.com/vinjan23/Mainnet/tree/main/8ball" target="_blank">
+   <a href="https://github.com/vinjan23/Mainnet/tree/main/Realio" target="_blank">
       <h5 style={{margin: 0, fontSize: '20px', color: '#FFFFFF'}}>SERVICE</h5>
     </a>
-    <a href="https://explorer.vinjan.xyz/arkhadian-mainnet/staking/8ballvaloper1jf02c7l9ezy30eflx0kgp54d0tpsj2zz3zmn8h" target="_blank">
+    <a href="https://explorer.vinjan.xyz/arkhadian-mainnet/staking/realiovaloper1csj5g5j4r682apvjckdk3qldkup4y07472gtkh" target="_blank">
     <h5 style={{margin: 0, fontSize: '20px', color: '#FFFFFF'}}>EXPLORER</h5>
     </a>
   </div>
@@ -120,16 +120,16 @@ export const Mainnet = () => {
 
 <div className="item">
   <div style={{display: 'flex', flexDirection: 'column', alignItems: 'center'}}>
-   <h3 style={{marginBottom: '10px'}}>realio</h3>
-   <a href="https://explorer.vinjan.xyz/realio-mainnet/staking/realiovaloper1csj5g5j4r682apvjckdk3qldkup4y07472gtkh" target="_blank">
+   <h3 style={{marginBottom: '10px'}}>arkhadian</h3>
+   <a href="https://explorer.vinjan.xyz/arkhadian-mainnet/staking/arkhvaloper1caclqqep2jprjn0evwyt3jkn6q3zxx4c80rzcj" target="_blank">
      <div style={{borderRadius: '50%', width: '150px', height: '150px', overflow: 'hidden'}}>
-        <img src={realio} alt="Image" style={{width: '100%', height: '100%'}} />
+        <img src={arkhadian} alt="Image" style={{width: '100%', height: '100%'}} />
       </div>
        </a>
-   <a href="https://github.com/vinjan23/Mainnet/tree/main/Realio" target="_blank">
+   <a href="https://github.com/vinjan23/Mainnet/tree/main/Arkh" target="_blank">
       <h5 style={{margin: 0, fontSize: '20px', color: '#FFFFFF'}}>SERVICE</h5>
     </a>
-    <a href="https://explorer.vinjan.xyz/arkhadian-mainnet/staking/realiovaloper1csj5g5j4r682apvjckdk3qldkup4y07472gtkh" target="_blank">
+    <a href="https://explorer.vinjan.xyz/arkhadian-mainnet/staking/arkhvaloper1caclqqep2jprjn0evwyt3jkn6q3zxx4c80rzcj" target="_blank">
     <h5 style={{margin: 0, fontSize: '20px', color: '#FFFFFF'}}>EXPLORER</h5>
     </a>
   </div>
@@ -137,16 +137,16 @@ export const Mainnet = () => {
 
 <div className="item">
   <div style={{display: 'flex', flexDirection: 'column', alignItems: 'center'}}>
-   <h3 style={{marginBottom: '10px'}}>planq</h3>
-   <a href="https://explorer.vinjan.xyz/planq-mainnet/staking/plqvaloper1csj5g5j4r682apvjckdk3qldkup4y0745djamf" target="_blank">
+   <h3 style={{marginBottom: '10px'}}>8 Ball</h3>
+   <a href="https://explorer.vinjan.xyz/8ball-mainnet/staking/8ballvaloper1jf02c7l9ezy30eflx0kgp54d0tpsj2zz3zmn8h" target="_blank">
      <div style={{borderRadius: '50%', width: '150px', height: '150px', overflow: 'hidden'}}>
-        <img src={planq} alt="Image" style={{width: '100%', height: '100%'}} />
+        <img src={eightball} alt="Image" style={{width: '100%', height: '100%'}} />
       </div>
        </a>
-   <a href="https://github.com/vinjan23/Mainnet/tree/main/Planq" target="_blank">
+   <a href="https://github.com/vinjan23/Mainnet/tree/main/8ball" target="_blank">
       <h5 style={{margin: 0, fontSize: '20px', color: '#FFFFFF'}}>SERVICE</h5>
     </a>
-    <a href="https://explorer.vinjan.xyz/arkhadian-mainnet/staking/plqvaloper1csj5g5j4r682apvjckdk3qldkup4y0745djamf" target="_blank">
+    <a href="https://explorer.vinjan.xyz/arkhadian-mainnet/staking/8ballvaloper1jf02c7l9ezy30eflx0kgp54d0tpsj2zz3zmn8h" target="_blank">
     <h5 style={{margin: 0, fontSize: '20px', color: '#FFFFFF'}}>EXPLORER</h5>
     </a>
   </div>
