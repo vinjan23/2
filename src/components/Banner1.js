37b81736c7576eeabe46b89dfdaef5a3cc135a7d
vinjan <108977419+vinjan23@users.noreply.github.com>
2023-05-25 11:16:10 +0700

    Update and rename Middle.js to Banner1.js

--- src/components/Banner1.js
+++ src/components/Banner1.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Container, Row, Col, Tab, Nav } from "react-bootstrap";
-import { TeamCard } from "./TeamCard";
+
 import Banner1 from "../assets/img/banner1.png";
 import colorSharp2 from "../assets/img/color-sharp2.png";
 import TrackVisibility from 'react-on-screen';
